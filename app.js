814c52e382329c29f93469f65bb295f7e017d260
zezutom <zezulatomas@gmail.com>
2014-05-27 23:36:24 +0200

    Improvements: * externalized config (yaml) * better response examples (http://jsonapi.org/format/) * parameterized requests support (REST)

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-// #1 Set up API mock, where responses will be loaded
-// from the filesystem
-var API_DIR = "/test/mocks/api";
-var DEFAULT_SERVER = "127.0.0.1";
-var DEFAULT_PORT = "8080";
+// Configuration
+var SERVER = require("config").Server;
+var ROUTES = require("config").Routes;
+
+// File search wiring
 var appRoot = __dirname.replace(/\\/g,"/"),
-    ipaddress = DEFAULT_SERVER,
-    port = DEFAULT_PORT,
-    mockRoot = appRoot + API_DIR,
-    suffix = ".json",
-    searchPattern = mockRoot + "/**/*" + suffix,
-    apiRoot = "/api",
+    mockRoot = appRoot + ROUTES.source,
+    searchPattern = mockRoot + "/**/*" + ROUTES.suffix,
     fs = require("fs"),
     glob = require("glob");
-var logger = require("morgan");
+var files = glob.sync(searchPattern);
 
-// #2 Create express app
+// Web server
 var express = require('express');
 var app = express();
 
-// #3 Configure logging and error handling
+// Logging
+var logger = require("morgan");
 app.use(logger('dev'));
 
-// #4 Search for stored files
-var files = glob.sync(searchPattern);
-
-// #5 Map each of the found files
+// File to API mappings
 if (files && files.length > 0) {
     files.forEach(function(fileName) {
-        var mapping = apiRoot + fileName
+        var mapping =  ROUTES.target + fileName
                                 .replace(mockRoot, "")
-                                .replace(suffix, "");
+                                .replace(ROUTES.suffix, "")
+                                .replace(/_/g, "/");
+
         app.get(mapping, function(req, res) {
             var data = fs.readFileSync(fileName, "utf8");
-            res.writeHead(200, {"Content-Type": "application/json"});
+            res.writeHead(200, {"Content-Type": ROUTES.type});
             res.write(data);
             res.end();
         });
@@ -42,9 +38,9 @@ if (files && files.length > 0) {
     console.log("No mappings found! Please check the configuration.");
 }
 
-// #6 Start the server
+// Bootstrap
 console.log("App root dir: [%s]", appRoot);
-console.log("Starting server: [http://%s:%s]", ipaddress, port);
-app.listen(port, ipaddress);
+console.log("Starting server: [http://%s:%s]", SERVER.host, SERVER.port);
+app.listen(SERVER.port, SERVER.host);
 
 module.exports = app;
