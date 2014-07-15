3ed764378b1ca82e07ebf24f5b916c739a509290
zezutom <zezulatomas@gmail.com>
2014-07-15 21:46:53 +0200

    Fixed a flaw in the config util (routes and views are directories on the config path and not props of the server object), improved start up script: can be started from any location.

--- modules/utils/conf.js
+++ modules/utils/conf.js
@@ -1,4 +1,5 @@
 var server = require("config").Server;
+var fs = require("fs");
 
 // Configuration directory: either it's provided via an env. variable,
 // or it defaults to the project root directory
@@ -6,10 +7,14 @@ var root = process.env.NODE_CONFIG_DIR || process.cwd();
 
 var ConfUtils = {
     views: function() {
-        return root + (server.views || "/views");
+        return this._path("views");
     },
     routes: function() {
-        return root + (server.routes || "/routes/index");
+        return this._path("routes/index.js");
+    },
+    _path: function(file) {
+        var path = root + "/" + file;
+        return (fs.existsSync(path)) ? path.replace("\.js", "") : undefined;
     },
     port: function() {
         return server.port;
--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
-# Adjust the config path to your needs 
-export NODE_CONFIG_DIR=~/workspace/nodejs/apimock-config/
+# export NODE_CONFIG_DIR=~/workspace/nodejs/apimock-config/
+
+# path to the configuration directory
+export NODE_CONFIG_DIR="$1"
+
+# script working directory
+swd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $swd
+
+# start express
 npm start
